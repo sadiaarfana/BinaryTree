333b8d3d26161051a03da8ddc2e56e9d6ce50faf
sadiaarfana <sadia14@gmail.com>
2018-07-12 21:59:27 +1000

    Add files via upload

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize variables
+            int j = 2;
+            int level, ball, SIZE = 2;
+
+            // Input Tree level
+            Console.Write("Level: ");
+            if (int.TryParse(Console.ReadLine(), out level))
+            {
+                // Input no of balls
+                Console.Write("Ball: ");
+                if (int.TryParse(Console.ReadLine(), out ball))
+                {
+                    // Calculate total nodes based on level
+                    Tree bst = new Tree(1);
+                    for (int i = 2; i <= level; i++)
+                    {
+                        j = j * 2;
+                        SIZE = SIZE + j;
+                    }
+                    SIZE += 1;
+
+                    // Build tree
+                    Tree builder = new Tree(SIZE);
+                    Node tree = builder.root;
+
+                    // Print tree
+                    //Console.WriteLine("Print Tree: ");
+                    //bst.print(tree, "");
+                    //Console.WriteLine();
+
+                    // Initialize array for containers in last level with value false. 
+                    // False means that container doesn't have any ball and true is vice versa                    
+                    var dictionary = new Dictionary<int, bool>();
+                    int minNode = SIZE - j + 1, maxNode= SIZE;
+
+                    for (int i = minNode; i <= maxNode; i++)
+                    {
+                        dictionary.Add(i, false);
+                    }
+
+                    // Prediction
+                    Console.WriteLine("Prediction: Below Containers  don't receive any ball");
+                    var r = new Random();
+                    var predictionList = new List<int>();
+                    var dictionaryList = dictionary.ToList();
+                    for (int i = 0; i < j - ball;)
+                    {
+                        var n = dictionaryList[r.Next(1, j)].Key;
+                        
+                        if (!predictionList.Contains(n))
+                        {
+                            predictionList.Add(n);
+                            Console.Write("{0}  ", predictionList[i++]-j+1);                            
+                        }
+                    }
+                    Console.WriteLine();
+
+                    // Tree traverse for each ball
+                    for (int i = 0; i < ball;)
+                    {
+                        bst.traverse(tree, ref dictionary, ref i);
+                    }
+
+                    // Take list of containers which don't have any ball.
+                    var nodes = dictionary.Where(a => a.Value == false).ToList();
+
+                    // Result Output
+                    // container no starts with 1 
+                    Console.WriteLine("Result: Below Containers don't receive any ball");
+                    foreach (var node in nodes)
+                    {
+                        Console.Write("{0}  ", node.Key-j+1);
+                    }
+                    Console.WriteLine();
+
+                    //Compare Prediction and Results in %
+                    var rightAnswers = nodes.Where(n => predictionList.Contains(n.Key)).ToList();
+                    double matchPercentage = ((double)rightAnswers.Count / (double)predictionList.Count) * 100;
+                    Console.WriteLine("Match: {0:F2}%", matchPercentage);
+                }
+                else Console.WriteLine("No valid input provided.");
+            }
+            else Console.WriteLine("No valid input provided.");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        public Node(int data)
+        {
+            ID = data;
+            IsBall = false;
+        }
+        public int ID;
+        public bool IsBall;
+        public Node Left;
+        public Node Right;
+    }
+
+    class Tree
+    {
+        public Node root { get; }
+        public Tree(int data)
+        {
+            root = buildTree(data, 1);
+        }
+
+        private Node buildTree(int data, int i)
+        {
+            if (i > data) return null;
+            Node next = new Node(i);
+            next.Left = buildTree(data, 2 * i);
+            next.Right = buildTree(data, 2 * i + 1);
+            return next;
+        }
+
+        public void traverse(Node root, ref Dictionary<int, bool> dictionary, ref int index)
+        {
+            if (root == null) return;
+
+            // If traverse reach at last node then ball will be in that container
+            if (root.Left == null && root.Right == null)
+            {
+                root.IsBall = true;
+                if (dictionary.ContainsKey(root.ID)) dictionary[root.ID] = true;
+                index++;
+                return;
+            }
+
+            // Update IsBall property based on node traverse.
+            if (dictionary != null)
+            {
+                if (dictionary.Any(d => d.Key == root.Left.ID && d.Value == true)) root.Left.IsBall = true;
+
+                if (dictionary.Any(d => d.Key == root.Right.ID && d.Value == true)) root.Right.IsBall = true;
+            }
+
+            // If child node already have balls then make root node IsBall true. 
+            if (root.Left.IsBall && root.Right.IsBall)
+            {
+                root.IsBall = true;
+                return;
+            }
+
+            // Randomly take open gate in node based on IsBall property
+            int gateOpen = -1;
+            if (!root.Left.IsBall && !root.Right.IsBall)
+            {
+                gateOpen = (new Random()).Next(0, 2);
+                if (gateOpen == 0) gateOpen = root.Left.ID;
+                else gateOpen = root.Right.ID;
+            }
+            else gateOpen = (root.Left.IsBall == true ? root.Right.ID : root.Left.ID);
+
+            // Traverse left / right node based on gateOpen
+            if (gateOpen == root.Left.ID) traverse(root.Left, ref dictionary, ref index);
+            else traverse(root.Right, ref dictionary, ref index);
+
+        }
+
+        public void print(Node root, String prefix)
+        {
+            if (root == null)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            Console.WriteLine(prefix + "+- " + root.ID);
+            print(root.Left, prefix + "|  ");
+            print(root.Right, prefix + "|  ");
+        }
+    }
+}
